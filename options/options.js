8de2d1ae79d87bf3b396ddfb2a91776968409686
Crowdbotics <engineering@crowdbotics.com>
2023-07-05 23:20:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-disk-dev-97982",
+  url: "https://little-disk-dev-97982.botics.co",
+  api: "https://little-disk-dev-97982.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
